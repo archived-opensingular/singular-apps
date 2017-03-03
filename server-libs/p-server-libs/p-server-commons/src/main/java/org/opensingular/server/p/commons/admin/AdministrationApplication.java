b1bb573edbe0e008302d25a736d1df627ad3711f
Vinicius Uriel <viniciusuriel@gmail.com>
2017-03-03 17:35:56 -0300

    corrigindo admin application

--- server-libs/p-server-libs/p-server-commons/src/main/java/org/opensingular/server/p/commons/admin/AdministrationApplication.java
+++ server-libs/p-server-libs/p-server-commons/src/main/java/org/opensingular/server/p/commons/admin/AdministrationApplication.java
@@ -8,11 +8,6 @@ import org.opensingular.server.p.commons.admin.healthsystem.HealthSystemPage;
 public class AdministrationApplication extends SingularApplication {
 
     @Override
-    protected String[] getPackagesToScan() {
-        return new String[]{"org.opensingular.server.commons.admin"};
-    }
-
-    @Override
     public Class<? extends Page> getHomePage() {
         return HealthSystemPage.class;
     }
