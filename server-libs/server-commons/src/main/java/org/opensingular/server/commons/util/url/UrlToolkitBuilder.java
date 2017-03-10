c4d627a21484bfa2ad52265e8b4688741f100420
Danilo Mesquita <danilofmesquita@gmail.com>
2017-03-10 19:46:30 -0300

    fix

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/util/url/UrlToolkitBuilder.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/util/url/UrlToolkitBuilder.java
@@ -2,7 +2,9 @@ package org.opensingular.server.commons.util.url;
 
 import org.apache.wicket.request.Url;
 
-public class UrlToolkitBuilder {
+import java.io.Serializable;
+
+public class UrlToolkitBuilder implements Serializable {
 
     public UrlToolkit build(Url url){
         return new UrlToolkit(url);
