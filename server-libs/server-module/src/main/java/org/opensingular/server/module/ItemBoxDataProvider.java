bc2e5ca823401b25c5365ae021aff7bb4c2006d8
Vinicius Uriel <viniciusuriel@gmail.com>
2017-03-16 15:39:18 -0300

    proposta de estrutura de caixas - fornecimento de dados.

--- server-libs/server-module/src/main/java/org/opensingular/server/module/ItemBoxDataProvider.java
+++ server-libs/server-module/src/main/java/org/opensingular/server/module/ItemBoxDataProvider.java
@@ -3,14 +3,16 @@ package org.opensingular.server.module;
 import org.opensingular.server.commons.persistence.filter.QuickFilter;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ItemBoxDataProvider {
 
 
-    List<ItemBoxData> search(QuickFilter filter);
-
+    List<Map<String, Object>> search(QuickFilter filter);
 
     Long count(QuickFilter filter);
+
+    void configureLineActions(ItemBoxData line);
 
 
 }
