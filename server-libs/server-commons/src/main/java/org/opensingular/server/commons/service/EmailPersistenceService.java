f9457156f0c14e79340424e9ff7134a586532911
Vinicius Uriel <viniciusuriel@gmail.com>
2017-04-05 13:55:49 -0300

    refatorando lancamento de excecao para novo formato

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/service/EmailPersistenceService.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/service/EmailPersistenceService.java
@@ -52,7 +52,7 @@ public class EmailPersistenceService implements IEmailService<Email>{
     public boolean send(Email email) {
         EmailEntity emailEntity = new EmailEntity();
         if (!validateRecipients(email.getAllRecipients())) {
-            throw SingularServerException.rethrow("O destinatário de e-mail é inválido.");
+            throw new SingularServerException("O destinatário de e-mail é inválido.");
         }
         emailEntity.setSubject(email.getSubject());
         emailEntity.setContent(email.getContent());
