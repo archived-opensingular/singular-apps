5b4b8c21ee4d367df5f6b51d96334f84adee2b2c
Vinicius Uriel <viniciusuriel@gmail.com>
2017-04-06 11:33:31 -0300

    ajustando itens do sonar

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/wicket/SingularAnnotatedMountScanner.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/wicket/SingularAnnotatedMountScanner.java
@@ -29,8 +29,9 @@ public class SingularAnnotatedMountScanner {
 
     private void validatePaths(List<Class<?>> classes) {
         Map<String, Class<?>> mountPaths = new HashMap<>();
+        List<String>          paths      = new ArrayList<>();
         for (Class<?> clazz : classes) {
-            List<String> paths = new ArrayList<>();
+            paths.clear();
             paths.add(clazz.getAnnotation(MountPath.class).value());
             paths.addAll(Arrays.asList(clazz.getAnnotation(MountPath.class).alt()));
             for (String path : paths) {
