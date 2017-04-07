4c438286f91e91753e1ed551cd5e90eff4a339dc
Vinicius Uriel <viniciusuriel@gmail.com>
2017-04-07 09:05:38 -0300

    renomeando dao de process group

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/persistence/dao/flow/ProcessGroupDAO.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/persistence/dao/flow/ProcessGroupDAO.java
@@ -18,26 +18,12 @@ package org.opensingular.server.commons.persistence.dao.flow;
 
 import org.opensingular.flow.persistence.entity.ProcessGroupEntity;
 import org.opensingular.lib.support.persistence.BaseDAO;
-import org.hibernate.criterion.Restrictions;
-
-import java.util.List;
 
 
-public class GrupoProcessoDAO extends BaseDAO<ProcessGroupEntity, String> {
+public class ProcessGroupDAO extends BaseDAO<ProcessGroupEntity, String> {
 
-    public GrupoProcessoDAO() {
+    public ProcessGroupDAO() {
         super(ProcessGroupEntity.class);
-    }
-
-    public List<ProcessGroupEntity> listarTodosGruposProcesso() {
-        return getSession().createCriteria(ProcessGroupEntity.class).list();
-    }
-
-    public ProcessGroupEntity findByName(String name) {
-        return (ProcessGroupEntity) getSession()
-                .createCriteria(ProcessGroupEntity.class)
-                .add(Restrictions.ilike("name", name))
-                .uniqueResult();
     }
 
 }
