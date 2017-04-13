b31f0a291c506e6295feabd0f656bf3d79a2e344
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-04-13 11:24:21 -0300

    Teste unitario MarkupCreatorTest, testes SDBHealth, alteração validatorOracleMock pra permitir o teste

--- server-libs/server-commons/src/test/java/org/opensingular/server/commons/test/ValidatorOracleMock.java
+++ server-libs/server-commons/src/test/java/org/opensingular/server/commons/test/ValidatorOracleMock.java
@@ -2,7 +2,6 @@ package org.opensingular.server.commons.test;
 
 import org.opensingular.server.commons.admin.healthsystem.validation.database.ValidatorOracle;
 import org.opensingular.server.commons.persistence.dto.healthsystem.ColumnInfoDTO;
-import org.opensingular.server.commons.persistence.dto.healthsystem.SequenceInfoDTO;
 import org.opensingular.server.commons.persistence.dto.healthsystem.TableInfoDTO;
 import org.springframework.context.annotation.Primary;
 
@@ -15,17 +14,42 @@ import java.util.List;
 @Named
 public class ValidatorOracleMock extends ValidatorOracle {
 
+    private boolean mockEncontradoBanco = true;
+
     protected List<ColumnInfoDTO> getColumnsInfoFromTable(String table) {
-        return null; // MOCK
+        return null;
     }
 
     protected List<String> getPermissionEspecificTable(String tabela) {
-        return Arrays.asList("SELECT", "UPDATE", "ALTER", "DELETE");
+        if(mockEncontradoBanco){
+            return Arrays.asList("SELECT", "UPDATE", "INSERT", "DELETE");
+        }else{
+            return Arrays.asList("SELECT", "UPDATE", "DELETE");
+        }
     }
 
     @Transactional
     @Override
     public void checkColumnPermissions(TableInfoDTO tableInfoDTO) {
-        // MOCK
+        // pra garantir que vai testar todos os casos de validacao.
+        if(mockEncontradoBanco){
+            mockEncontradoBanco = false;
+            tableInfoDTO.getColumnsInfo().forEach(column->{
+                column.setFoundDataBase(true);
+                column.setFoundHibernate(true);
+            });
+        }else{
+            mockEncontradoBanco = true;
+            boolean mockEncontradoHibernate = true;
+            for(ColumnInfoDTO columnInfo : tableInfoDTO.getColumnsInfo()){
+                if(mockEncontradoHibernate){
+                    columnInfo.setFoundHibernate(mockEncontradoHibernate);
+                    mockEncontradoHibernate = !mockEncontradoHibernate;
+                }else{
+                    columnInfo.setFoundHibernate(mockEncontradoHibernate);
+                    mockEncontradoHibernate = !mockEncontradoHibernate;
+                }
+            }
+        }
     }
 }
