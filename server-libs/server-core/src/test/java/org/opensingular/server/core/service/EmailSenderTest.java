bd6958c4fabb4be0ecc4d93ff21853df3c8d3a65
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-04-24 15:10:06 -0300

    Testes EmailSender, emailSenderScheduledJob

---
+++ server-libs/server-core/src/test/java/org/opensingular/server/core/service/EmailSenderTest.java
@@ -0,0 +1,85 @@
+package org.opensingular.server.core.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opensingular.server.commons.persistence.entity.email.EmailAddresseeEntity;
+import org.opensingular.server.commons.service.dto.Email;
+import org.opensingular.server.core.test.SingularServerBaseTest;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Date;
+
+public class EmailSenderTest extends SingularServerBaseTest {
+
+    @Inject
+    private EmailSenderScheduledJob emailSenderJob;
+
+    @Inject
+    private EmailSender emailSender;
+
+    @Test
+    @Transactional
+    public void testJobWithoutEmailToSend(){
+        Object run = emailSenderJob.run();
+        Assert.assertEquals("0 sent from total of 0", run);
+
+        Assert.assertEquals("EmailSenderScheduledJob [getScheduleData()="
+                +emailSenderJob.getScheduleData().toString()+", getId()="+emailSenderJob.getId()+"]",
+                emailSenderJob.toString());
+    }
+
+    @Test
+    public void testNotSendEmail(){
+        emailSender.setHost(null);
+        Assert.assertFalse(emailSender.send((Email.Addressee) null));
+    }
+
+    @Test
+    public void sendEmailExceptionTest(){
+        emailSender.setHost("opensingular.org");
+
+        Date date = new Date();
+
+        Email email = createMockEmail();
+        EmailAddresseeEntity entity = createMockEmailAddresseeEntity(date);
+
+        Email.Addressee addressee = new Email.Addressee(email, entity);
+
+        Assert.assertFalse(emailSender.send(addressee));
+
+        addressee.setSentDate(date);
+        Assert.assertEquals(date, addressee.getSentDate());
+
+        entity.setSentDate(date);
+        Assert.assertEquals(date, entity.getSentDate());
+    }
+
+    private EmailAddresseeEntity createMockEmailAddresseeEntity(Date date) {
+        EmailAddresseeEntity emailEntity = new EmailAddresseeEntity();
+        emailEntity.setCod((long)1);
+        Assert.assertEquals(new Long(1), emailEntity.getCod());
+
+        Assert.assertNull(emailEntity.getAddress());
+        Assert.assertNull(emailEntity.getAddresseType());
+
+        emailEntity.setAddress("mirante.teste@gmail.com");
+        return emailEntity;
+    }
+
+    private Email createMockEmail() {
+        Email email = new Email();
+        email.withSubject("Test");
+        email.withContent("Some context to test.");
+        email.addTo(Arrays.asList("email1@email.com", "email2@email.com", "email3@email.com"));
+
+        email.addCc(Arrays.asList("email1@email.com", "email2@email.com", "email3@email.com"));
+        email.addCc("email1@email.com", "email2@email.com", "email3@email.com");
+
+        email.addBcc(Arrays.asList("email1@email.com", "email2@email.com", "email3@email.com"));
+        email.addBcc("email1@email.com", "email2@email.com", "email3@email.com");
+
+        return email;
+    }
+}
