362c604714577110521ad630897a82ed09f35db6
Vinicius Uriel <viniciusuriel@gmail.com>
2017-05-05 11:58:15 -0300

    log de exceções

--- server-libs/server-commons/src/main/java/org/opensingular/server/p/commons/admin/healthsystem/validation/webchecker/IpChecker.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/p/commons/admin/healthsystem/validation/webchecker/IpChecker.java
@@ -1,23 +1,25 @@
 package org.opensingular.server.p.commons.admin.healthsystem.validation.webchecker;
 
-import java.net.InetAddress;
-
 import org.opensingular.form.type.core.SIString;
 import org.opensingular.form.validation.IInstanceValidatable;
+import org.opensingular.lib.commons.util.Loggable;
 
-public class IpChecker implements IProtocolChecker {
+import java.net.InetAddress;
 
-	@Override
-	public void protocolCheck(IInstanceValidatable<SIString> validatable) {
-		String url = validatable.getInstance().getValue().replace("ip://", "");
-		String[] piecesUrl = url.split(":");
-		
-		try {
-			if(!InetAddress.getByName(piecesUrl[0]).isReachable(2000)){
-				throw new Exception("Address not reacheble!");
-			}
-		} catch (Exception e) {
-			validatable.error(e.getMessage());
-		}
-	}
+public class IpChecker implements IProtocolChecker, Loggable {
+
+    @Override
+    public void protocolCheck(IInstanceValidatable<SIString> validatable) {
+        String   url       = validatable.getInstance().getValue().replace("ip://", "");
+        String[] piecesUrl = url.split(":");
+
+        try {
+            if (!InetAddress.getByName(piecesUrl[0]).isReachable(2000)) {
+                throw new Exception("Address not reacheble!");
+            }
+        } catch (Exception e) {
+            getLogger().error(e.getMessage(), e);
+            validatable.error(e.getMessage());
+        }
+    }
 }
