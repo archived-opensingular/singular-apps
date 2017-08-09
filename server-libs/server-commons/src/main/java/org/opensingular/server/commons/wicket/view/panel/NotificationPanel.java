27fe73e88cc482901f27b7502b79c5b2a459dc12
Vinicius Uriel <viniciusuriel@gmail.com>
2017-08-09 17:35:31 -0300

    adicionando comportamento padrão ao notification panel

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/wicket/view/panel/NotificationPanel.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/wicket/view/panel/NotificationPanel.java
@@ -4,19 +4,17 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.ContentDisposition;
 import org.opensingular.form.wicket.link.FileDownloadLink;
+import org.opensingular.lib.commons.dto.HtmlToPdfDTO;
 import org.opensingular.lib.commons.pdf.HtmlToPdfConverter;
+import org.opensingular.lib.wicket.util.model.IReadOnlyModel;
 import org.opensingular.server.commons.service.PetitionInstance;
-import org.opensingular.server.p.commons.PServerFreeMarkerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class NotificationPanel<PI extends PetitionInstance> extends Panel {
@@ -39,7 +37,14 @@ public class NotificationPanel<PI extends PetitionInstance> extends Panel {
     }
 
     protected IModel<File> getPdfModel(IModel<Pair<String, String>> model, IModel<PI> modelOfpetition) {
-        return new Model<>();
+        return new IReadOnlyModel<File>() {
+            @Override
+            public File getObject() {
+                HtmlToPdfDTO dto = new HtmlToPdfDTO();
+                dto.setBody(model.getObject().getValue());
+                return htmlToPdfConverter.convert(dto).orElse(null);
+            }
+        };
     }
 
 }
