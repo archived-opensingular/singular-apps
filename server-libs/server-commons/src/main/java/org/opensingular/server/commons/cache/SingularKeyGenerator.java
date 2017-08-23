5cd095362f25452238da41add1f4d578ab41f0ca
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-23 19:08:22 -0300

    Melhorando Key Generator

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/cache/SingularKeyGenerator.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/cache/SingularKeyGenerator.java
@@ -1,9 +1,12 @@
 package org.opensingular.server.commons.cache;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,14 +15,23 @@ public class SingularKeyGenerator extends SimpleKeyGenerator {
 
     @Override
     public Object generate(Object target, Method method, Object... params) {
-        if (params.length == 0) {
-            String methodName       = method.getName();
-            String methodReturnType = method.getReturnType().getName();
-            String parameters       = Stream.of(method.getParameters()).map(Parameter::getName).collect(Collectors.joining());
-            return (methodName.hashCode() + methodReturnType.hashCode() + parameters.hashCode()) / 2;
-        } else {
-            return super.generate(target, method, params);
+        String methodName = method.getName();
+        String methodReturnType = method.getReturnType().getName();
+        Parameter[] methodParameters = method.getParameters();
+        return generateKey(methodName, methodReturnType, Stream.of(methodParameters).map(Parameter::getName), params);
+    }
+
+    public static String generateKey(String methodName, String methodReturnType, String[] parameters, Object[] params) {
+        int paramsHashCode = 0;
+        if (params != null) {
+            paramsHashCode = Arrays.hashCode(params);
         }
+        BigInteger big = BigInteger.ZERO;
+        big = big.add(BigInteger.valueOf(methodName.hashCode()));
+        big = big.add(BigInteger.valueOf(methodReturnType.hashCode()));
+        big = big.add(BigInteger.valueOf(Arrays.hashCode(parameters)));
+        big = big.add(BigInteger.valueOf(paramsHashCode));
+        return methodName + big.hashCode();
     }
 
 }
---
+++ server-libs/server-commons/src/test/java/org/opensingular/server/commons/cache/SingularKeyGeneratorTest.java
@@ -0,0 +1,36 @@
+package org.opensingular.server.commons.cache;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class SingularKeyGeneratorTest {
+
+    @Test
+    public void testSameMethodWithParameters() throws Exception {
+        String key_1 = SingularKeyGenerator.generateKey("findByKey", "MyDTO", new String[]{"Integer"}, new Integer[]{1});
+        String key_2 = SingularKeyGenerator.generateKey("findByKey", "MyDTO", new String[]{"Integer"}, new Integer[]{2});
+        String key_3 = SingularKeyGenerator.generateKey("findByKey", "MyDTO", new String[]{"Integer"}, new Integer[]{3});
+        assertNotEquals(key_1, key_2);
+        assertNotEquals(key_1, key_3);
+        assertNotEquals(key_2, key_3);
+    }
+
+    @Test
+    public void testManyMethods() throws Exception {
+        String key_1 = SingularKeyGenerator.generateKey("findEntityByKey", "MyEntity", new String[]{"Integer"}, new Integer[]{0});
+        String key_2 = SingularKeyGenerator.generateKey("findDTOByKey", "MyDTO", new String[]{"Integer"}, new Integer[]{0});
+        String key_3 = SingularKeyGenerator.generateKey("getName", "String", null, null);
+        assertNotEquals(key_1, key_2);
+        assertNotEquals(key_1, key_3);
+        assertNotEquals(key_2, key_3);
+    }
+
+    @Test
+    public void testSameMethodWithSameParams() throws Exception {
+        String key_1 = SingularKeyGenerator.generateKey("findEntityByKey", "MyEntity", new String[]{"Integer"}, new Integer[]{0});
+        String key_2 = SingularKeyGenerator.generateKey("findEntityByKey", "MyEntity", new String[]{"Integer"}, new Integer[]{0});
+        assertEquals(key_1, key_2);
+    }
+}
