44c768f52da09197009448b591eaa8cfcc989649
Danilo Mesquita <danilofmesquita@gmail.com>
2017-10-04 17:40:57 -0300

    refatorando studio para permitir incluir paineis wickets aleatorios

--- server-libs/server-studio-app/src/main/java/org/opensingular/server/studio/wicket/ServerStudioApplication.java
+++ server-libs/server-studio-app/src/main/java/org/opensingular/server/studio/wicket/ServerStudioApplication.java
@@ -19,7 +19,7 @@ import org.opensingular.studio.core.config.StudioConfig;
 import org.opensingular.studio.core.config.StudioConfigProvider;
 import org.opensingular.studio.core.view.StudioFooter;
 import org.opensingular.studio.core.view.StudioHeader;
-import org.opensingular.studio.core.view.StudioPortalPage;
+import org.opensingular.studio.core.view.StudioPage;
 import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
 
 import java.nio.charset.StandardCharsets;
@@ -35,7 +35,7 @@ public class ServerStudioApplication extends SingularServerApplication {
 
     @Override
     public Class<? extends Page> getHomePage() {
-        return StudioPortalPage.class;
+        return StudioPage.class;
     }
 
     @Override
