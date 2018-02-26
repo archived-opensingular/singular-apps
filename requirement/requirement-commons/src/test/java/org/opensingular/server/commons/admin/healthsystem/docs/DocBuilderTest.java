efd9c6de8b2c2518fc3cc01917e4f4b9ae95b752
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-02-26 15:53:47 -0300

    [BUGFIX] fixing test classes wrong packages

--- requirement/requirement-commons/src/test/java/org/opensingular/server/commons/admin/healthsystem/docs/DocBuilderTest.java
+++ requirement/requirement-commons/src/test/java/org/opensingular/server/commons/admin/healthsystem/docs/DocBuilderTest.java
@@ -22,10 +22,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.opensingular.form.SDictionary;
 import org.opensingular.form.SType;
-import org.opensingular.server.commons.admin.healthsystem.docs.DocBlock;
-import org.opensingular.server.commons.admin.healthsystem.docs.DocFieldMetadata;
-import org.opensingular.server.commons.admin.healthsystem.docs.DocTable;
-import org.opensingular.server.commons.admin.healthsystem.docs.DocumentationMetadataBuilder;
 
 import java.util.LinkedHashSet;
 
