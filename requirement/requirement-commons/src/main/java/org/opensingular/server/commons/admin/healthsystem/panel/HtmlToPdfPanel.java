d7633369cb4fc96ef4b2642c174243da82611d7a
italo.ferreira <italo.ferreira@mirante.net.br>
2018-03-20 13:06:42 -0300

    [SGL-573]Flexibilizando passagem de parametros para o dto do PDF. Inclusão da funcionalidade de testar localmente o HtmlToPdf.

--- requirement/requirement-commons/src/main/java/org/opensingular/server/commons/admin/healthsystem/panel/HtmlToPdfPanel.java
+++ requirement/requirement-commons/src/main/java/org/opensingular/server/commons/admin/healthsystem/panel/HtmlToPdfPanel.java
@@ -2,9 +2,16 @@ package org.opensingular.server.commons.admin.healthsystem.panel;
 
 import de.alpharogroup.wicket.js.addon.toastr.ToastrType;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.IRequestCycle;
+import org.apache.wicket.request.handler.resource.ResourceStreamRequestHandler;
 import org.apache.wicket.request.resource.ContentDisposition;
-import org.opensingular.form.wicket.link.FileDownloadLink;
+import org.apache.wicket.util.file.Files;
+import org.apache.wicket.util.resource.FileResourceStream;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.time.Duration;
 import org.opensingular.form.wicket.panel.SingularFormPanel;
 import org.opensingular.lib.commons.dto.HtmlToPdfDTO;
 import org.opensingular.lib.commons.pdf.HtmlToPdfConverter;
@@ -14,11 +21,15 @@ import org.opensingular.server.commons.admin.healthsystem.stypes.STypePdfHealth;
 import org.opensingular.server.commons.wicket.view.SingularToastrHelper;
 import org.opensingular.ws.wkhtmltopdf.client.RestfulHtmlToPdfConverter;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.util.Optional;
-import java.util.UUID;
+
 
 public class HtmlToPdfPanel extends Panel implements Loggable {
+
+    @Inject
+    private HtmlToPdfConverter htmlToPdfConverter;
 
     public HtmlToPdfPanel(String id) {
         super(id);
@@ -29,35 +40,66 @@ public class HtmlToPdfPanel extends Panel implements Loggable {
         super.onInitialize();
         Form form = new Form("formExport");
         SingularFormPanel singularFormPanel = new SingularFormPanel("htmlExport", STypePdfHealth.class);
-        FileDownloadLink link = new FileDownloadLink("exportHtml", ContentDisposition.ATTACHMENT, String.format("temp-%s.pdf", UUID.randomUUID())) {
+        form.add(new PDFDownloadLink("exportRest", form ,(IModel<SIPdfHealth>) singularFormPanel.getInstanceModel()) {
             @Override
-            public void onClick() {
-                final File file = getConvertedFile((SIPdfHealth) singularFormPanel.getInstance());
-                if (file != null) {
-                    super.downloadFile(file);
-                } else {
-                    getLogger().info("Não foi possível converter o PDF");
-                    new SingularToastrHelper(this).
-                            addToastrMessage(ToastrType.ERROR, "Não foi possível fazer o donwload do PDF");
-                }
+            File getConvertedFile(SIPdfHealth instance) {
+                final HtmlToPdfConverter converter = new RestfulHtmlToPdfConverter(instance.getEndpoint());
+                final HtmlToPdfDTO htmlToPdfDTO = new HtmlToPdfDTO(instance.getHtmlToExport());
+                htmlToPdfDTO.addParam("--title");
+                htmlToPdfDTO.addParam("PDF de Teste");
+                final Optional<File> convert = converter.convert(htmlToPdfDTO);
+                return convert.orElse(null);
             }
-        };
+        });
+        form.add(new PDFDownloadLink("exportLocal", form, (IModel<SIPdfHealth>) singularFormPanel.getInstanceModel()) {
+            @Override
+            File getConvertedFile(SIPdfHealth instance) {
+                final Optional<File> convert = htmlToPdfConverter.convert(new HtmlToPdfDTO(instance.getHtmlToExport()));
+                return convert.orElse(null);
+            }
+        });
         form.add(singularFormPanel);
-        form.add(link);
         add(form);
     }
 
-    private File getConvertedFile(SIPdfHealth instance) {
-        final HtmlToPdfConverter converter = new RestfulHtmlToPdfConverter(instance.getEndpoint());
-        final HtmlToPdfDTO htmlToPdfDTO = new HtmlToPdfDTO(instance.getHtmlToExport());
-        htmlToPdfDTO.addParam("--title");
-        htmlToPdfDTO.addParam("PDF de Teste");
-        htmlToPdfDTO.addParam("--print-media-type");
-        htmlToPdfDTO.addParam("--load-error-handling");
-        htmlToPdfDTO.addParam("ignore");
-        final Optional<File> convert = converter.convert(htmlToPdfDTO);
-        return convert.orElse(null);
+    private abstract class PDFDownloadLink extends SubmitLink {
+
+        private IModel<SIPdfHealth> modelOfInstance;
+
+        PDFDownloadLink(String id, Form<?> form, IModel<SIPdfHealth> model) {
+            super(id, form);
+            this.modelOfInstance = model;
+        }
+
+        @Override
+        public void onSubmit() {
+            super.onSubmit();
+            final File file = getConvertedFile(modelOfInstance.getObject());
+            if (file == null) {
+                getLogger().info("Não foi possível converter o PDF");
+                new SingularToastrHelper(this).
+                        addToastrMessage(ToastrType.ERROR, "Não foi possível fazer o donwload do PDF");
+                return;
+            }
+            final IResourceStream resourceStream = new FileResourceStream(new org.apache.wicket.util.file.File(file));
+            getRequestCycle().scheduleRequestHandlerAfterCurrent(
+                    new ResourceStreamRequestHandler(resourceStream) {
+                        @Override
+                        public void respond(IRequestCycle requestCycle) {
+                            super.respond(requestCycle);
+                            Files.remove(file);
+                        }
+                    }
+                            .setFileName(file.getName())
+                            .setCacheDuration(Duration.NONE)
+                            .setContentDisposition(ContentDisposition.ATTACHMENT));
+        }
+
+        abstract File getConvertedFile(SIPdfHealth instance);
+
     }
+
+
 }
 
 
--- requirement/requirement-commons/src/main/java/org/opensingular/server/commons/admin/healthsystem/stypes/STypePdfHealth.java
+++ requirement/requirement-commons/src/main/java/org/opensingular/server/commons/admin/healthsystem/stypes/STypePdfHealth.java
@@ -5,12 +5,10 @@ import org.opensingular.form.STypeComposite;
 import org.opensingular.form.TypeBuilder;
 import org.opensingular.form.type.core.STypeHTML;
 import org.opensingular.form.type.core.STypeString;
-import org.opensingular.form.validation.validator.StringValidators;
-import org.opensingular.lib.commons.base.SingularProperties;
-import org.opensingular.ws.wkhtmltopdf.constains.HtmlToPdfConstants;
+import org.opensingular.form.view.SViewByRichText;
+import org.opensingular.ws.wkhtmltopdf.client.RestfulHtmlToPdfConverter;
 
 import javax.annotation.Nonnull;
-import java.util.Optional;
 
 @SInfoType(spackage = SSystemHealthPackage.class, newable = true, name = "pdfhealth")
 public class STypePdfHealth extends STypeComposite<SIPdfHealth> {
@@ -24,15 +22,14 @@ public class STypePdfHealth extends STypeComposite<SIPdfHealth> {
 
     @Override
     protected void onLoadType(@Nonnull TypeBuilder tb) {
-        final Optional<String> endereco = SingularProperties.getOpt(HtmlToPdfConstants.ENDPOINT_WS_WKHTMLTOPDF);
         endpoint = this.addFieldString("endpoint");
-        endereco.ifPresent(url -> endpoint.setInitialValue(url));
-        endpoint.addInstanceValidator(StringValidators.isNotBlank("Endpoint obrigatório"));
+        endpoint.setInitialValue(RestfulHtmlToPdfConverter.getEndpointDefault());
         endpoint.asAtr()
                 .label("Endpoint:")
                 .maxLength(155)
                 .enabled(true);
         htmlToExport = this.addField("htmlToExport", STypeHTML.class);
+        htmlToExport.withView(SViewByRichText::new);
         htmlToExport.asAtr().label("HTML Exportação");
     }
 }
