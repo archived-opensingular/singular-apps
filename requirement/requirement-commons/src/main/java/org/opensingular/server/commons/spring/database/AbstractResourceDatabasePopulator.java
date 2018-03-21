6895768c9aa3699565746e48ff73c7565036af45
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-03-21 15:51:42 -0300

    [SGL-547] - Correcao dos scripts para cada dialeto especifico.

---
+++ requirement/requirement-commons/src/main/java/org/opensingular/server/commons/spring/database/AbstractResourceDatabasePopulator.java
@@ -0,0 +1,39 @@
+package org.opensingular.server.commons.spring.database;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class AbstractResourceDatabasePopulator extends ResourceDatabasePopulator {
+
+    @Value("classpath:db/ddl/create-schema-drop-all.sql")
+    private Resource sqlDropAndCreateSchema;
+
+    @Value("classpath:db/ddl/create-tables.sql")
+    private Resource sqlCreateTables;
+
+    @Value("classpath:db/dml/insert-flow-data.sql")
+    private Resource insertSingularData;
+
+    private List<Resource> scripts = new ArrayList<>();
+
+    @PostConstruct
+    public void init(){
+        setSqlScriptEncoding(StandardCharsets.UTF_8.name());
+        super.addScript(sqlDropAndCreateSchema);
+        super.addScript(sqlCreateTables);
+        super.addScript(insertSingularData);
+        scripts.forEach(s -> super.addScript(s));
+        scripts.clear();
+    }
+
+    @Override
+    public void addScript(Resource script) {
+        scripts.add(script);
+    }
+}
