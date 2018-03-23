1798ad3ca51f632c98b676caa2f2b66fc004fa52
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-03-23 16:43:33 -0300

    [SGL-587] - Correcao nos scripts

---
+++ requirement/requirement-commons/src/main/resources/db/ddl/create-table-actor.sql
@@ -0,0 +1,17 @@
+/*Esse Script é um default, provavelmente deverá ser alterado para suportar o ator especifico do cliente.*/
+
+/*==============================================================*/
+/* Table: TB_ATOR                                               */
+/*==============================================================*/
+CREATE TABLE DBSINGULAR.TB_ATOR (
+   CO_ATOR  BIGINT       IDENTITY,
+   CO_USUARIO           VARCHAR(60)          NOT NULL,
+  CONSTRAINT PK_ATOR PRIMARY KEY (CO_ATOR)
+);
+
+/*==============================================================*/
+/* View: VW_ATOR                                    */
+/*==============================================================*/
+create view DBSINGULAR.VW_ATOR AS
+SELECT A.CO_ATOR, A.CO_USUARIO as CO_USUARIO, A.CO_USUARIO as NO_ATOR, ' ' as DS_EMAIL
+FROM DBSINGULAR.TB_ATOR A;
--- requirement/requirement-commons/src/main/resources/db/ddl/sqlserver/create-function.sql
+++ requirement/requirement-commons/src/main/resources/db/ddl/sqlserver/create-function.sql
@@ -1,14 +1,13 @@
 CREATE FUNCTION DBSINGULAR.TO_CHAR(@DATE_VAL DATETIME, @FORMAT VARCHAR(20))
-RETURNS VARCHAR(17)
+  RETURNS VARCHAR(17)
 AS
-BEGIN
+  BEGIN
     DECLARE @ret VARCHAR(17);
     IF(@FORMAT = 'DD/MM/YYYY HH24:MI')
-          SELECT @ret = convert(varchar(10),@DATE_VAL,103) + ' ' + cast(DATEPART(HOUR,@DATE_VAL) as varchar(2)) + ':' + cast(DATEPART(MINUTE,@DATE_VAL)    as varchar(2))
+      SELECT @ret = convert(varchar(10),@DATE_VAL,103) + ' ' + cast(DATEPART(HOUR,@DATE_VAL) as varchar(2)) + ':' + cast(DATEPART(MINUTE,@DATE_VAL)    as varchar(2))
     ELSE IF (@FORMAT = 'dd/MM/yyyy')
-          SELECT @ret = convert(varchar(10),@DATE_VAL,103)
+      SELECT @ret = convert(varchar(10),@DATE_VAL,103)
     RETURN @ret;
-END;
-
-
+  END
+GO
 
