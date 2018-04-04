101f98b11c4090c77e1d0934d24e7a30c0149768
Delfino <delfinogomes@gmail.com>
2018-04-04 16:09:55 -0300

    [SGL-602] - Implementando restrição de segurança para não permitir detalhamento de requerimento por usuário que tenha o link direto.

--- requirement/requirement-module/src/main/java/org/opensingular/requirement/module/wicket/view/util/dispatcher/DispatcherPage.java
+++ requirement/requirement-module/src/main/java/org/opensingular/requirement/module/wicket/view/util/dispatcher/DispatcherPage.java
@@ -36,12 +36,13 @@ import org.opensingular.form.SFormUtil;
 import org.opensingular.form.SType;
 import org.opensingular.form.wicket.enums.ViewMode;
 import org.opensingular.lib.commons.util.Loggable;
+import org.opensingular.requirement.commons.SingularRequirement;
 import org.opensingular.requirement.commons.exception.SingularServerException;
 import org.opensingular.requirement.commons.flow.SingularRequirementTaskPageStrategy;
 import org.opensingular.requirement.commons.flow.SingularWebRef;
 import org.opensingular.requirement.commons.form.FormAction;
 import org.opensingular.requirement.commons.persistence.entity.form.RequirementEntity;
-import org.opensingular.requirement.commons.SingularRequirement;
+import org.opensingular.requirement.commons.service.RequirementInstance;
 import org.opensingular.requirement.commons.service.RequirementService;
 import org.opensingular.requirement.commons.service.SingularRequirementService;
 import org.opensingular.requirement.commons.spring.security.AuthorizationService;
@@ -59,6 +60,7 @@ import org.opensingular.requirement.commons.wicket.view.util.ActionContext;
 
 import javax.inject.Inject;
 import java.lang.reflect.Constructor;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.opensingular.lib.wicket.util.util.WicketUtils.$b;
@@ -228,12 +230,17 @@ public class DispatcherPage extends WebPage implements Loggable {
 
         SingularUserDetails userDetails = SingularSession.get().getUserDetails();
 
+        Long requirementId = context.getRequirementId().orElse(null);
         boolean hasPermission = authorizationService.hasPermission(
-                context.getRequirementId().orElse(null),
+                requirementId,
                 context.getFormName().orElse(null),
                 String.valueOf(userDetails.getUserPermissionKey()),
                 context.getFormAction().map(FormAction::name).orElse(null)
         );
+
+        if (requirementId != null) {
+            hasPermission &= isOwner(userDetails, requirementId);
+        }
 
         // Qualquer modo de edição o usuário deve ter permissão e estar alocado na tarefa,
         // para os modos de visualização basta a permissão.
@@ -243,6 +250,12 @@ public class DispatcherPage extends WebPage implements Loggable {
             return hasPermission;
         }
 
+    }
+
+    protected boolean isOwner(SingularUserDetails userDetails, Long requirementId) {
+        String applicantId = userDetails.getApplicantId();
+        RequirementInstance requirement = requirementService.getRequirement(requirementId);
+        return Objects.equals(requirement.getApplicant().getIdPessoa(), applicantId);
     }
 
     private boolean isViewModeEdit(ActionContext context) {
