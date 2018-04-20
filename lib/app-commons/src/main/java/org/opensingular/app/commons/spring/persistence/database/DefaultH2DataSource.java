f0c0c1627e3e6ec87fbdd277bcb0dc163a769677
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-04-20 15:26:28 -0300

    [SGL_662] - Correção do Header, remoção do sidebar-toggler nas paginas que não deveria existir, e correção do layout de logout do usuario

--- lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/database/DefaultH2DataSource.java
+++ lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/database/DefaultH2DataSource.java
@@ -39,6 +39,7 @@ public class DefaultH2DataSource extends DelegatingDataSource implements Loggabl
         setAutoServer(true);
         setCacheSize(4096);
         setMultiThreaded(true);
+        setMode("ORACLE");
         setLockTimeout(15000);
     }
 
