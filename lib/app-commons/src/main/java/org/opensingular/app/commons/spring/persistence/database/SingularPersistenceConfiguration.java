513c595ade41d5830f080d9c6abdd2fc6dda71de
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-06-13 18:21:13 -0300

    [SGL-688] improving default parameter name

--- lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/database/SingularPersistenceConfiguration.java
+++ lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/database/SingularPersistenceConfiguration.java
@@ -36,10 +36,10 @@ public interface SingularPersistenceConfiguration {
      * Configure hibernates packages to scan in addition to the singular platform packages, ie: singular entities packages
      * is already configured by the platform.
      *
-     * @param configuratePackagesToScan A configuration class that contains a pre-populated list with singular default packages,
+     * @param packagesConfig A configuration class that contains a pre-populated list with singular default packages,
      *                                  cleaning or removing pre-populated data in this list would led to platform malfunction.
      */
-    void configureHibernatePackagesToScan(PackageScanConfiguration configuratePackagesToScan);
+    void configureHibernatePackagesToScan(PackageScanConfiguration packagesConfig);
 
     /**
      * Method responsible to configure the list of scripts files that will be execute after the creation of Entities by Hibernate.
