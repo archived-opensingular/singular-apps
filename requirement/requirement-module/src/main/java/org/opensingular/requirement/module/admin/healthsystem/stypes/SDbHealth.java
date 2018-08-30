da8967b7c4428bf66fbbfa4bd05fe8d89c106610
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-08-30 10:20:55 -0300

    [SGL-742] - Melhoria na api dos componentes de lista

--- requirement/requirement-module/src/main/java/org/opensingular/requirement/module/admin/healthsystem/stypes/SDbHealth.java
+++ requirement/requirement-module/src/main/java/org/opensingular/requirement/module/admin/healthsystem/stypes/SDbHealth.java
@@ -53,7 +53,7 @@ public class SDbHealth extends STypeComposite<SIComposite> {
     protected void onLoadType(@Nonnull TypeBuilder tb) {
 
         tablesList = this.addFieldListOfComposite("tablesList", "tabela");
-        tablesList.withView(() -> new SViewListByMasterDetail().fullSize().disableNew().configureDeleteButtonPerRow(f -> false));
+        tablesList.withView(() -> new SViewListByMasterDetail().fullSize().disableNew().disableDelete());
 
         STypeComposite<SIComposite> table = tablesList.getElementsType();
 
@@ -89,13 +89,13 @@ public class SDbHealth extends STypeComposite<SIComposite> {
                 .enabled(true)
                 .asAtrBootstrap()
                 .colPreference(2);
-        userPrivs.withView(() -> new SViewListByTable().disableNew().configureDeleteButtonPerRow(f -> false));
+        userPrivs.withView(() -> new SViewListByTable().disableNew().disableDelete());
 
         table.addInstanceValidator(this::tableValidation);
 
         columnsInfo = table.addFieldListOfComposite("columnsInfo", "column");
 
-        columnsInfo.withView(() -> new SViewListByTable().disableNew().configureDeleteButtonPerRow(f -> false));
+        columnsInfo.withView(() -> new SViewListByTable().disableNew().disableDelete());
         columnsInfo.asAtr().label("Colunas");
 
         STypeComposite<SIComposite> column = columnsInfo.getElementsType();
