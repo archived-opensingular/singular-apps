846b055318bc5d8d982dd5225094faeb7148a00c
Thiago Bomfim <thiago.bomfim@opensingular.com>
2018-09-12 11:26:01 -0300

    [SINGULEI-10] - Alterando o SessionLocator e fazendo o simpleDao utiliza ele, para ser um bean configuravel para obter a session.

--- requirement/requirement-module/src/main/java/org/opensingular/requirement/module/spring/SingularDefaultBeanFactory.java
+++ requirement/requirement-module/src/main/java/org/opensingular/requirement/module/spring/SingularDefaultBeanFactory.java
@@ -16,6 +16,7 @@
 
 package org.opensingular.requirement.module.spring;
 
+import org.hibernate.SessionFactory;
 import org.opensingular.app.commons.mail.persistence.dao.EmailAddresseeDao;
 import org.opensingular.app.commons.mail.persistence.dao.EmailDao;
 import org.opensingular.app.commons.mail.schedule.SingularSchedulerBean;
@@ -54,6 +55,7 @@ import org.opensingular.lib.commons.base.SingularProperties;
 import org.opensingular.lib.commons.context.spring.SpringServiceRegistry;
 import org.opensingular.lib.commons.pdf.HtmlToPdfConverter;
 import org.opensingular.lib.commons.scan.SingularClassPathScanner;
+import org.opensingular.lib.support.persistence.SessionLocator;
 import org.opensingular.lib.support.spring.security.DefaultRestUserDetailsService;
 import org.opensingular.lib.support.spring.security.RestUserDetailsService;
 import org.opensingular.requirement.module.cache.SingularKeyGenerator;
@@ -513,6 +515,11 @@ public class SingularDefaultBeanFactory {
             }
         }
         return null;
+    }
+
+    @Bean
+    public SessionLocator sessionProvider(SessionFactory sessionFactory){
+        return () -> sessionFactory.getCurrentSession();
     }
 
 }
