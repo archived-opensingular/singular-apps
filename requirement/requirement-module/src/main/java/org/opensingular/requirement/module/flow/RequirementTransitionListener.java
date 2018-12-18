dc6609d30d54c41ae1329b39f5274d37ddf29481
Delfino <delfinogomes@gmail.com>
2018-12-18 01:42:15 -0200

    [SINGULAR-REQUIREMENT] - Documentando métodos, criando utilitário para decodificar string em Base64

--- requirement/requirement-module/src/main/java/org/opensingular/requirement/module/flow/RequirementTransitionListener.java
+++ requirement/requirement-module/src/main/java/org/opensingular/requirement/module/flow/RequirementTransitionListener.java
@@ -7,11 +7,24 @@ import org.opensingular.flow.core.ITransitionListener;
  */
 public interface RequirementTransitionListener extends ITransitionListener<RequirementTransitionContext> {
 
+    /**
+     * Possibilita interceptar a execução de uma transição, no caso do singular-server
+     * isso ocorre após a consolidação dos rascunhos.
+     *
+     * @param requirementTransitionContext
+     */
     @Override
     default void beforeTransition(RequirementTransitionContext requirementTransitionContext){
 
     }
 
+    /**
+     * Possibilita interceptar a execução de uma transição antes da consolidação dos rascunhos,
+     * caso seja necessário alterar o conteúdo de um formulátio que esteja como rascunho antes que
+     * ele seja persistido como um versão.
+     *
+     * @param requirementTransitionContext
+     */
     default void beforeConsolidateDrafts(RequirementTransitionContext requirementTransitionContext) {
 
     }
