f4ecb3b1807d0e6a0f8c8cd91ebfd628a37028b9
Thiago Bomfim <thiago.bomfim@opensingular.com>
2019-01-31 13:21:55 -0200

    [ICMBIO-257] - Implementação da logica para conseguir recuperar a ultima mensagem de exceção no login

--- requirement/requirement-module/src/main/java/org/opensingular/requirement/module/spring/security/config/LoginPage.java
+++ requirement/requirement-module/src/main/java/org/opensingular/requirement/module/spring/security/config/LoginPage.java
@@ -16,21 +16,28 @@
 
 package org.opensingular.requirement.module.spring.security.config;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.opensingular.lib.commons.util.Loggable;
 import org.opensingular.lib.wicket.util.template.admin.SingularAdminTemplate;
+import org.springframework.security.core.AuthenticationException;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Login page used for Singular.
  */
 public class LoginPage extends SingularAdminTemplate implements Loggable {
 
+    private String messageError;
     private boolean hasError = false;
 
     public LoginPage() {
@@ -40,7 +47,33 @@ public class LoginPage extends SingularAdminTemplate implements Loggable {
     public LoginPage(PageParameters parameters) {
         super(parameters);
         hasError = parameters != null && parameters.get("error") != null;
-        add(new LoginForm("loginForm"));
+        populateSpringErrorMessage();
+        createContainerFormLogin();
+    }
+
+    protected Component createExtraButtonsContainer(String id){
+       return new WebMarkupContainer(id).setVisible(false);
+    }
+
+    protected Component createLoginTitle(String id){
+       return new Label(id, "Login");
+    }
+
+    private void createContainerFormLogin() {
+        WebMarkupContainer container = createContainerLogin("container-login");
+        LoginForm loginForm = createLoginForm("loginForm");
+        loginForm.add(createExtraButtonsContainer("extraButtons"));
+        loginForm.add(createLoginTitle("login-title"));
+        container.add(loginForm);
+        add(container);
+    }
+
+    protected WebMarkupContainer createContainerLogin(String id) {
+        return new WebMarkupContainer(id);
+    }
+
+    protected LoginForm createLoginForm(String id) {
+        return new LoginForm(id);
     }
 
     @Override
@@ -58,11 +91,12 @@ public class LoginPage extends SingularAdminTemplate implements Loggable {
         return false;
     }
 
-    private class LoginForm extends Form<Void> {
+    protected class LoginForm extends Form<Void> {
 
         LoginForm(String id) {
             super(id);
             WebMarkupContainer feedBackMessage = new WebMarkupContainer("feedback");
+            feedBackMessage.add(new Label("messageError", messageError));
             feedBackMessage.setVisible(hasError);
             add(feedBackMessage);
 
@@ -70,6 +104,14 @@ public class LoginPage extends SingularAdminTemplate implements Loggable {
             Model<String> password = new Model<>();
             add(new RequiredTextField<>("username", username).setLabel(new Model<>("Login")));
             add(new PasswordTextField("password", password).setLabel(new Model<>("Senha")));
+        }
+    }
+
+    private void populateSpringErrorMessage() {
+        HttpServletRequest httpRequest  = (HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest();
+        Object springSecurityLastException = httpRequest.getSession().getAttribute("SPRING_SECURITY_LAST_EXCEPTION");
+        if(springSecurityLastException != null) {
+            messageError = ((AuthenticationException) httpRequest.getSession().getAttribute("SPRING_SECURITY_LAST_EXCEPTION")).getMessage();
         }
     }
 
