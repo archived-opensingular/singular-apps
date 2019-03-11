3469565d8266be16b29c0a43d3678189ab838737
edgar.filho <edgar.filho@opensingular.com>
2019-03-11 16:36:22 -0300

    Modificando filtro de Série do documento

--- requirement/requirement-sei-30-connector/src/main/java/org/opensingular/requirement/connector/sei30/model/Serie.java
+++ requirement/requirement-sei-30-connector/src/main/java/org/opensingular/requirement/connector/sei30/model/Serie.java
@@ -4,11 +4,6 @@ import java.io.Serializable;
 
 public class Serie implements Serializable {
 
-    public static final String DOC_INTERNO_EXTERNO = "T";
-    public static final String DOC_INTERNO = "I";
-    public static final String DOC_EXTERNO = "E";
-    public static final String FORMULARIO = "F";
-
     private String id;
     private String nome;
     private String aplicabilidade;
@@ -23,6 +18,10 @@ public class Serie implements Serializable {
         this(id, nome, null);
     }
 
+    public static SerieType[] getAplicabilidadeWithModelo() {
+        return new SerieType[] {SerieType.DOC_INTERNO, SerieType.DOC_INTERNO};
+    }
+
     public String getNome() {
         return nome;
     }
@@ -33,5 +32,33 @@ public class Serie implements Serializable {
 
     public String getAplicabilidade() {
         return aplicabilidade;
+    }
+
+    public enum SerieType {
+        DOC_INTERNO_EXTERNO("T"),
+        DOC_INTERNO("I"),
+        DOC_EXTERNO("E"),
+        FORMULARIO("F");
+
+
+        private String description;
+
+        SerieType(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public static SerieType valueOfEnumByDescription(String description) {
+            for (SerieType type : SerieType.values()) {
+                if (description.trim().equals(type.getDescription())) {
+                    return type;
+                }
+            }
+            return null;
+        }
+
     }
 }
