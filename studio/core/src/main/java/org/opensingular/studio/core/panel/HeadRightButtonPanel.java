d6b1c80848e818b14e8d6bf98450d51d70c564a7
EdgarFabiano <eddy.led@gmail.com>
2019-04-04 14:55:16 -0300

    Adicionando Behaviour de title para o link

--- studio/core/src/main/java/org/opensingular/studio/core/panel/HeadRightButtonPanel.java
+++ studio/core/src/main/java/org/opensingular/studio/core/panel/HeadRightButtonPanel.java
@@ -7,6 +7,8 @@ import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.opensingular.studio.core.panel.button.IHeaderRightButton;
 
+import static org.opensingular.lib.wicket.util.util.WicketUtils.$b;
+
 public class HeadRightButtonPanel extends Panel {
 
     private final IHeaderRightButton headerRightButton;
@@ -26,7 +28,6 @@ public class HeadRightButtonPanel extends Panel {
     protected void onInitialize() {
         super.onInitialize();
 
-
         AbstractLink link = headerRightButton.createButton("headerRightAction");
         Label btnLabel = new Label("headerRightActionLabel", headerRightButton.getLabel());
 
@@ -37,6 +38,8 @@ public class HeadRightButtonPanel extends Panel {
                 tag.put("class", headerRightButton.getIcon());
             }
         };
+
+        link.add($b.onComponentTag((c, tag) -> tag.put("title", headerRightButton.getTitle())));
         link.add(btnLabel);
         link.add(btnIcon);
         link.setVisible(headerRightButton.isVisible());
