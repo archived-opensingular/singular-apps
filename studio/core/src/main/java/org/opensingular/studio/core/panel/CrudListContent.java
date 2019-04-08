15b2a5edd3bacba8100ad85d371698647ea1d933
Thiago Bomfim <thiago.bomfim@opensingular.com>
2019-04-08 09:25:49 -0300

    [BUG-FIX] - Correção no load do javascript referente ao SingularFormPanel.

--- studio/core/src/main/java/org/opensingular/studio/core/panel/CrudListContent.java
+++ studio/core/src/main/java/org/opensingular/studio/core/panel/CrudListContent.java
@@ -20,6 +20,8 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -27,6 +29,7 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.resource.JQueryPluginResourceReference;
 import org.opensingular.form.SInstance;
 import org.opensingular.form.wicket.component.BFModalWindow;
 import org.opensingular.form.wicket.component.SingularValidationButton;
@@ -63,6 +66,16 @@ public class CrudListContent extends CrudShellContent {
 
     public CrudListContent(CrudShellManager crudShellManager) {
         this(crudShellManager, new CrudListConfig());
+    }
+
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        /*É necessário carregar o script do SingularFormPanel, pois como ele está dentro da modal,
+         o renderHead do panel ainda não foi chamado, o que estava gerando ao abrir a modal.*/
+        response.render(JavaScriptHeaderItem
+                .forReference(new JQueryPluginResourceReference(SingularFormPanel.class, "SingularFormPanel.js")));
     }
 
     public CrudListContent(CrudShellManager crudShellManager, CrudListConfig crudListConfig) {
@@ -259,7 +272,7 @@ public class CrudListContent extends CrudShellContent {
         private SingularFormPanel filterPanel;
 
 
-        public HeaderFilterAction(){
+        public HeaderFilterAction() {
             createFilterPanel();
         }
 
@@ -343,7 +356,7 @@ public class CrudListContent extends CrudShellContent {
 
         private void createFilterPanel() {
             filterPanel = new SingularFormPanel("filterPanel", getCrudShellManager().getStudioDefinition().getFilterType());
-            filterPanel.setNested(true);;
+            filterPanel.setNested(true);
 
         }
 
