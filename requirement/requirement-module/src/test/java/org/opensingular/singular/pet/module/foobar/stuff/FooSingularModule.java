fb85294db9c57899e4c1495f2000bfe8d3e4f19f
Thiago Bomfim <thiago.bomfim@opensingular.com>
2019-04-12 08:24:13 -0300

    [REVERT] - Revert comment.

--- requirement/requirement-module/src/test/java/org/opensingular/singular/pet/module/foobar/stuff/FooSingularModule.java
+++ requirement/requirement-module/src/test/java/org/opensingular/singular/pet/module/foobar/stuff/FooSingularModule.java
@@ -1,79 +1,78 @@
-///*
-// *
-// *  * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
-// *  *
-// *  * Licensed under the Apache License, Version 2.0 (the "License");
-// *  *  you may not use this file except in compliance with the License.
-// *  * You may obtain a copy of the License at
-// *  *
-// *  * http://www.apache.org/licenses/LICENSE-2.0
-// *  *
-// *  * Unless required by applicable law or agreed to in writing, software
-// *  * distributed under the License is distributed on an "AS IS" BASIS,
-// *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// *  * See the License for the specific language governing permissions and
-// *  * limitations under the License.
-// *
-// */
-//
-//package org.opensingular.singular.pet.module.foobar.stuff;
-//
-//import org.opensingular.requirement.module.RequirementRegistry;
-//import org.opensingular.requirement.module.SingularModule;
-//
-//import org.opensingular.requirement.module.config.DefaultContexts;
-//import org.opensingular.requirement.module.config.workspace.Workspace;
-//import org.opensingular.requirement.module.workspace.DefaultDraftbox;
-//import org.opensingular.requirement.module.workspace.DefaultInbox;
-//import org.opensingular.requirement.module.workspace.DefaultOngoingbox;
-//import org.opensingular.requirement.module.workspace.WorkspaceRegistry;
-//
-//public class FooSingularModule implements SingularModule {
-//    public static final String GRUPO_TESTE = "GRUPO_TESTE";
-//
-//    @Override
-//    public String abbreviation() {
-//        return GRUPO_TESTE;
-//    }
-//
-//    @Override
-//    public String name() {
-//        return "Grupo Processo Teste";
-//    }
-//
-//    @Override
-//    public void requirements(RequirementRegistry requirementRegistry) {
-//        requirementRegistry
-//                .add(FooRequirement.class);
-//    }
-//
-//    @Override
-//    public void workspace(WorkspaceRegistry workspaceRegistry) {
-//        workspaceRegistry
-//                .add(FooWorklistContext.class)
-//                .add(FooRequirementContext.class);
-//    }
-//
-//
-//    public static class FooWorklistContext extends DefaultContexts.WorklistContext {
-//        @Override
-//        public void configure(Workspace workspace) {
-//            workspace
-//                    .menu()
-//                    .addCategory("", category -> category
-//                            .addBox(DefaultInbox.class, box -> box.newFor(FooRequirement.class))
-//                            .addBox(DefaultDraftbox.class));
-//        }
-//    }
-//
-//    public static class FooRequirementContext extends DefaultContexts.RequirementContext {
-//        @Override
-//        public void configure(Workspace workspace) {
-//            workspace
-//                    .menu()
-//                    .addCategory("", category -> category
-//                            .addBox(DefaultDraftbox.class, box -> box.newFor(FooRequirement.class))
-//                            .addBox(DefaultOngoingbox.class));
-//        }
-//    }
-//}
+/*
+ *
+ *  * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.opensingular.singular.pet.module.foobar.stuff;
+
+import org.opensingular.requirement.module.RequirementRegistry;
+import org.opensingular.requirement.module.SingularModule;
+import org.opensingular.requirement.module.config.DefaultContexts;
+import org.opensingular.requirement.module.config.workspace.Workspace;
+import org.opensingular.requirement.module.workspace.DefaultDraftbox;
+import org.opensingular.requirement.module.workspace.DefaultInbox;
+import org.opensingular.requirement.module.workspace.DefaultOngoingbox;
+import org.opensingular.requirement.module.workspace.WorkspaceRegistry;
+
+public class FooSingularModule implements SingularModule {
+    public static final String GRUPO_TESTE = "GRUPO_TESTE";
+
+    @Override
+    public String abbreviation() {
+        return GRUPO_TESTE;
+    }
+
+    @Override
+    public String name() {
+        return "Grupo Processo Teste";
+    }
+
+    @Override
+    public void requirements(RequirementRegistry requirementRegistry) {
+        requirementRegistry
+                .add(FooRequirement.class);
+    }
+
+    @Override
+    public void workspace(WorkspaceRegistry workspaceRegistry) {
+        workspaceRegistry
+                .add(FooWorklistContext.class)
+                .add(FooRequirementContext.class);
+    }
+
+
+    public static class FooWorklistContext extends DefaultContexts.WorklistContext {
+        @Override
+        public void configure(Workspace workspace) {
+            workspace
+                    .menu()
+                    .addCategory("", category -> category
+                            .addBox(DefaultInbox.class, box -> box.newFor(FooRequirement.class))
+                            .addBox(DefaultDraftbox.class));
+        }
+    }
+
+    public static class FooRequirementContext extends DefaultContexts.RequirementContext {
+        @Override
+        public void configure(Workspace workspace) {
+            workspace
+                    .menu()
+                    .addCategory("", category -> category
+                            .addBox(DefaultDraftbox.class, box -> box.newFor(FooRequirement.class))
+                            .addBox(DefaultOngoingbox.class));
+        }
+    }
+}
