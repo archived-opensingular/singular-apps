cf1f88b9d280f8cd81e57c125627ddcc53655594
Thiago Bomfim <thiago.bomfim@opensingular.com>
2019-08-02 12:01:55 -0300

    [SINGULEI-201] - Movendo o ServerAttachmentPersistenceService para o app-commons, e alterando o serviço do Singulei para utilizar esse service.

--- lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/attachment/ServerAttachmentPersistenceService.java
+++ lib/app-commons/src/main/java/org/opensingular/app/commons/spring/persistence/attachment/ServerAttachmentPersistenceService.java
@@ -1,24 +1,20 @@
 /*
- * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *   Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
-package org.opensingular.requirement.module.service.attachment;
-
-import java.util.Optional;
-import javax.inject.Inject;
-import javax.transaction.Transactional;
+package org.opensingular.app.commons.spring.persistence.attachment;
 
 import org.opensingular.form.document.SDocument;
 import org.opensingular.form.persistence.dto.AttachmentRef;
@@ -29,7 +25,11 @@ import org.opensingular.form.service.IFormService;
 import org.opensingular.form.type.core.attachment.AttachmentCopyContext;
 import org.opensingular.form.type.core.attachment.IAttachmentRef;
 import org.opensingular.form.type.core.attachment.helper.IAttachmentPersistenceHelper;
-import org.opensingular.requirement.module.exception.SingularServerException;
+import org.opensingular.lib.commons.base.SingularException;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 public class ServerAttachmentPersistenceService<T extends AttachmentEntity, C extends AttachmentContentEntity> extends ServerAbstractAttachmentPersistenceService<T, C> {
@@ -75,7 +75,7 @@ public class ServerAttachmentPersistenceService<T extends AttachmentEntity, C ex
     public void deleteAttachment(String id, SDocument document) {
         formAttachmentService.deleteFormAttachmentEntity(getAttachmentEntity(id),
                 formService.findCurrentFormVersion(document)
-                        .orElseThrow(() -> new SingularServerException("FormVersion não encontrado")));
+                        .orElseThrow(() -> new SingularException("FormVersion não encontrado")));
     }
 
     @Override
